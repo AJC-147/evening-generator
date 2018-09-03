ccaec247829486cc60473d7889e46530813c4221
Irene R <irene@smallwondermedia.com>
2018-09-03 14:03:44 -0400

    8

---
+++ assets/js/script-irene.js
@@ -0,0 +1,51 @@
+// var queryURL = "https://api.edamam.com/search?app_id=c5fa5243&app_key=1374f025ec2d733d2010bcbfe317e079&q=chicken";
+
+// var movies = $("#dropDown option:selected").text();
+
+// $(document).ready(function() {
+
+function findMovie(movie) {
+    var queryUrl = "https://www.omdbapi.com/?apikey=d9666985&t=scarface";
+    console.log(queryURL);
+
+    // $.ajax({
+    //     url: queryURL,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
+};
+// });
+
+// $(document).ready(function() {
+
+//     // on click submit button
+//     $("#submit").click(function() {
+
+//         // submit button retrieves #dropDown option:selected
+//         $("#dropDown option:selected").text();
+//         // alert($("#dropDown option:selected").text());
+
+//         var selection = $(this).attr("data-select");
+//         var queryUrl = "http://www.omdbapi.com/?apikey=d9666985&t=" + selection;
+
+//         $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//         }).then(function(response) {
+//             )};
+//         console.log(response);
+
+//     })
+// });
+
+    
+
+
+// $(function() {
+//     $('#interstellar').change(function() {
+//         this.form.submit();
+//     });
+// });
+
+
