0a768dd8ae7fbcde794f699c72a6a0d473ba96b6
Irene R <irene@smallwondermedia.com>
2018-09-06 12:22:27 -0400

    5

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,66 +51,7 @@ $(document).ready(function() {
             resultDiv.append(recipeLink);
 
             $("#recipe").append(resultDiv);
-        });
-        
-        
-//        var selection = $("#dropDown option:selected").text();
-//        var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + selection;
-//        console.log(selection);
-//
-//        $.ajax({
-//            url: queryURL,
-//            method: "GET"
-//        }).then(function(response) {
-//            console.log(response);
-//            $("#foodResult").empty();
-//
-//            var resultDiv = $("<div class='foodResult'>");
-//
-//            var chosenMeal = response.meals[Math.floor(Math.random() * response.meals.length)];
-//
-//            var imgURL = chosenMeal.strMealThumb;
-//            var image = $("<img>").attr("src", imgURL);
-//
-//            var name = chosenMeal.strMeal;
-//            var foodName = $("<p>").text(name);
-//            resultDiv.append(foodName);
-//
-//            // console.log(recipeId);
-//            var recipeId = chosenMeal.idMeal;
-//            var recipeLink = $("<a />", {
-//                name : "link",
-//                href : "https://www.themealdb.com/meal.php?c=" + recipeId,
-//                text : "Recipe"
-//            });
-//            console.log(recipeLink);
-//
-//            resultDiv.prepend(image);
-//            $(image).wrap("<a href=" + "https://www.themealdb.com/meal.php?c=" + recipeId +" target='_blank'></a>");
-//            resultDiv.append(recipeLink);
-//
-//            $("#foodResult").append(resultDiv);
-//        });
-
-        
-        // var movieArray = ["Jurassic Park", "Inception", "The Muppets", "Inside Out", "Gravity", "Slumdog Millionaire"];
-        // var randomMovie = movieArray[Math.floor(Math.random()*movieArray.length)];
-
-        // $.ajax({
-        //     url: "https://www.omdbapi.com/?apikey=d9666985&t=" + randomMovie,
-        //     method: "GET"
-        // }).then(function(response) {
-        //    $("#movieResult").empty();
-
-        //    var movieDiv = $("<div class='movie'>");
-
-        //     var imgURL = response.Poster;
-        //     var image = $("<img>").attr("src", imgURL);
-        //     movieDiv.prepend(image);
-
-
-        //    $("#movieResult").append(movieDiv);
-        // });
+        }); 
 
 
         // DYNAMIC MOVIE CODE
@@ -138,15 +79,30 @@ $(document).ready(function() {
             var imgURL = movieSuggestion.Poster;
             var image = $("<img>").attr("src", imgURL);
             movieDiv.prepend(image);
+            // $(image).wrap("<a href=" +  "https://www.imdb.com/title/" + imdb + "/plotsummary?ref_=tt_ql_stry_2", target='_blank'></a>");
+            // movieDiv.append(recipeLink);
 
-            var title = movieSuggestion.Title;
-            var titleText = $("<p>").text(title);
-            movieDiv.append(titleText);
 
             var plot = movieSuggestion.Plot;
             var plotText = $("<p>").text(plot);
             movieDiv.append(plotText);
 
+            var year = movieSuggestion.Year;
+            var yearText = $("<p>").text(year);
+            movieDiv.append(yearText);
+
+            var imdb = movieSuggestion.imdbID;
+            // console.log(imdb);
+            var imdbURL = "https://www.imdb.com/title/" + imdb + "/plotsummary?ref_=tt_ql_stry_2";
+            var imdbText = $("<a />", {
+                name : "link",
+                href : imdbURL,
+                text : "Learn more at IMDB",
+                target : "_blank"
+            })
+            movieDiv.append(imdbText);
+
+            https://www.imdb.com/title/" + imdb + "/plotsummary?ref_=tt_ql_stry_2"
 
             $("#movieResult").append(movieDiv);
         });
