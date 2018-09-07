28a8e04ea4c3c86639b1fa58b36e0b423f1b039f
alexander.chacko <alexander.chacko@outlook.com>
2018-09-06 23:47:21 -0400

    foodandmovie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
 
     $("#submit").click(function() {
-        var keyword = $("#foodGenre option:selected").text();
-        var ingredientNum = $("#ingredientNumb option:selected").text();
-        var queryURL = "https://api.edamam.com/search?&app_id=c5fa5243&app_key=1374f025ec2d733d2010bcbfe317e079&q=" + keyword + "&ingr=" + ingredientNum;
-        console.log(keyword);
-        console.log(ingredientNum);
+        
+        var keyword = $("#foodKeyword").val().trim();
+        var ingredientNum = $("#ingredientNumber input:checked").val();
+        var cookingTime = $("#cookTime input:checked").val();
+        var foodExclusions = $("#healthExclusions input:checked").val();
+        var queryURL = "https://api.edamam.com/search?&app_id=c5fa5243&app_key=1374f025ec2d733d2010bcbfe317e079&q=" + keyword + "&ingr=" + ingredientNum + "&time=" + cookingTime + foodExclusions;
+        console.log(queryURL);
+//        console.log(keyword);
+//        console.log(ingredientNum);
+//        console.log(cookingTime);
+//        console.log(foodExclusions);
 
         $.ajax({
             url: queryURL,
@@ -13,27 +19,27 @@ $(document).ready(function() {
         }).then(function(response) {
             console.log(response);
             
-            $("#recipe").empty();
+            $("#recipeResults").empty();
 
-            var resultDiv = $("<div class='recipe'>");
+            var recipeDiv = $("<div class='recipe'>");
 
             var randomRecipe = response.hits[Math.floor(Math.random() * response.hits.length)];
-            console.log(randomRecipe);
+//            console.log(randomRecipe);
             
             var name = randomRecipe.recipe.label;
             var recipeTitle = $("<h3>").text(name);
-            resultDiv.append(recipeTitle);
-            console.log(name);
-            console.log(recipeTitle);
+            recipeDiv.append(recipeTitle);
+//            console.log(name);
+//            console.log(recipeTitle);
 
             var imgURL = randomRecipe.recipe.image;
             var image = $("<img>").attr("src", imgURL);
-            console.log(image);
+//            console.log(image);
             
             var ingred = randomRecipe.recipe.ingredientLines;
             var ingredients = $("<p>").text(ingred);
-            resultDiv.append(ingredients);
-            console.log(ingredients);
+            recipeDiv.append(ingredients);
+//            console.log(ingredients);
 
 var source = randomRecipe.recipe.source;
             var recipeURL = randomRecipe.recipe.url;
@@ -46,81 +52,59 @@ var source = randomRecipe.recipe.source;
             console.log(recipeURL);
             console.log(recipeLink);
 
-            resultDiv.prepend(image);
+            recipeDiv.prepend(image);
             $(image).wrap("<a href=" + recipeURL +" target='_blank'></a>");
-            resultDiv.append(recipeLink);
+            recipeDiv.append(recipeLink);
 
-            $("#recipe").append(resultDiv);
+            $("#recipeResults").append(recipeDiv);
         });
         
         
-//        var selection = $("#dropDown option:selected").text();
-//        var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?a=" + selection;
-//        console.log(selection);
-//
-//        $.ajax({
-//            url: queryURL,
-//            method: "GET"
-//        }).then(function(response) {
-//            console.log(response);
-//            $("#foodResult").empty();
-//
-//            var resultDiv = $("<div class='foodResult'>");
-//
-//            var chosenMeal = response.meals[Math.floor(Math.random() * response.meals.length)];
-//
-//            var imgURL = chosenMeal.strMealThumb;
-//            var image = $("<img>").attr("src", imgURL);
-//
-//            var name = chosenMeal.strMeal;
-//            var foodName = $("<p>").text(name);
-//            resultDiv.append(foodName);
-//
-//            // console.log(recipeId);
-//            var recipeId = chosenMeal.idMeal;
-//            var recipeLink = $("<a />", {
-//                name : "link",
-//                href : "https://www.themealdb.com/meal.php?c=" + recipeId,
-//                text : "Recipe"
-//            });
-//            console.log(recipeLink);
-//
-//            resultDiv.prepend(image);
-//            $(image).wrap("<a href=" + "https://www.themealdb.com/meal.php?c=" + recipeId +" target='_blank'></a>");
-//            resultDiv.append(recipeLink);
-//
-//            $("#foodResult").append(resultDiv);
-//        });
-
         
-        var movieArray = ["Jurassic Park", "Inception", "The Muppets", "Inside Out", "Gravity", "Slumdog Millionaire"];
-        var randomMovie = movieArray[Math.floor(Math.random()*movieArray.length)];
+        
+        var sortBy = $("#sortedBy input:checked").val();
+        var queryURL = "https://api.themoviedb.org/3/discover/movie?api_key=af5bef9fa0d9daa3815142e331d7fa37&language=en-US&include_adult=false&include_video=false&vote_count.gte=500" + "&sort_by=" + sortBy;
+        console.log(queryURL);
+//        console.log(sortBy);
+        
+        
 
         $.ajax({
-            url: "https://www.omdbapi.com/?apikey=d9666985&t=" + randomMovie,
+            url: queryURL,
             method: "GET"
         }).then(function(response) {
-           $("#movieResult").empty();
+            console.log(response);
+            
+            $("#movieResult").empty();
 
-           var movieDiv = $("<div class='movie'>");
+            var movieDiv = $("<div class='movie'>");
 
-            var imgURL = response.Poster;
-            var image = $("<img>").attr("src", imgURL);
-            movieDiv.prepend(image);
+            var randomMovie = response.results[Math.floor(Math.random() * response.results.length)];
+//            console.log(randomMovie);
+            
+            
+            
+            var filmTitle = randomMovie.title;
+            var movieTitle = $("<h3>").text(filmTitle);
+            movieDiv.append(movieTitle);
+//            console.log(filmTitle);
+//            console.log(movieTitle);
+            
+            var posterURL = randomMovie.poster_path;
+            var imagePost = $("<img>").attr("src", "https://image.tmdb.org/t/p/w1280" + posterURL);
+            movieDiv.prepend(imagePost);
+//            console.log(imagePost);
+            
+            var summary = randomMovie.overview;
+            var plotSummary = $("<p>").text(summary);
+            movieDiv.append(plotSummary);
+//            console.log(plotSummary);
 
 
-           $("#movieResult").append(movieDiv);
+            $("#movieResult").append(movieDiv);
         });
-
-
-        
-
-
+          
 
     })
 
-
-
 });
-
-
